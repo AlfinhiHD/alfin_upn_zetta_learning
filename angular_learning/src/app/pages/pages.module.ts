8e3000f9dcde3747cb0bb564da031417b94db0db
AlfinhiHD <alfinhihd@gmail.com>
2022-11-02 13:57:48 +0700

    test

---
+++ angular_learning/src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserlistComponent } from './userlist/userlist.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserlistComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
