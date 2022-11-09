a781c22eab614695f94119af66247dfa51d978db
AlfinhiHD <alfinhihd@gmail.com>
2022-11-09 18:45:14 +0700

    push day 5

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -7,8 +7,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeroComponent } from './hero/hero.component';
 import { NewHeroComponent } from './new-hero/new-hero.component';
 import { HeroService } from './hero.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { LoggingService } from './logging.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialExampleModule } from '../material.module'
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +28,15 @@ import { LoggingService } from './logging.service';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MaterialExampleModule
+    
   ],
   providers: [HeroService, LoggingService],
   bootstrap: [AppComponent]
